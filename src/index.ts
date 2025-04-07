2948b0fe0456c9e1e9f7ddf680027f413b4d36c0
serarevetria <serafinrevetria@gmail.com>
2025-04-07 20:06:08 -0300

    feat: add Docker configuration and database connection setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { pool } from "./config/db";
 
 dotenv.config();
 
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (_req, res) => {
+  testDB();
   res.send("working");
 });
 
@@ -14,3 +16,12 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Running on port: ${PORT}`);
 });
+
+async function testDB() {
+  try {
+    const [rows] = await pool.query("SELECT 1 + 1 AS result");
+    console.log("connected with db", rows);
+  } catch (error) {
+    console.error("error with db:", error);
+  }
+}
